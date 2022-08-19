8c528f9405af2077e9ff786dc18dd8dd0e9d3cb3
Kpop124 <79905078+Kpop124@users.noreply.github.com>
2022-08-19 13:18:29 +0530

    Add files via upload

--- JS.js
+++ JS.js
@@ -2,6 +2,8 @@ left_wristX = ""
 left_wristY = ""
 right_wristX = ""
 right_wristY = ""
+score_left_wrist = ""
+status = ""
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -19,9 +21,6 @@ function setup() {
 
 function gotPoses(results) {
     if (results.length > 0) {
-        console.log("error");
-    }
-} else {
     console.log(results);
     left_wristX = results[0].pose.leftWrist.x;
     left_wristY = results[0].pose.leftWrist.y;
@@ -29,6 +28,7 @@ function gotPoses(results) {
     right_wristX = results[0].pose.rightWrist.x;
     right_wristY = results[0].pose.rightWrist.y;
     console.log("right wrist x: " + rightWristX + "right wrist y: " + rightWristY);
+    score_left_wrist = results[0].pose.keypoints[0].score;
 }
 
 function modelLoaded() {
@@ -37,4 +37,22 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    stroke("FB91FB");
+    fill("F5C4F5");
+    status = document.getElementById("song1");
+    if (score_left_wrist > 0.2) {
+        circle(left_wristX, left_wristY, 30);
+
+        function stop() {
+            song2.stop();
+        }
+    }
+    if (status = false) {
+        function play {
+            song1.play();
+            song1.setVolume(1)
+            song1.rate(1);
+        }
+    }
+    document.getElementById("song_name").innerHTML = "Song: " + song1
 }
